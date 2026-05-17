4ab6abcdece0faa6f3f9f814a5d8d688d87aa58e
renan.silva <renan.silva@sankhya.com.br>
2026-05-17 12:22:06 -0300

    Pipeline com eslint

--- temperatura.test.js
+++ temperatura.test.js
@@ -1,7 +1,7 @@
 const celsiusParaFahrenheit = require('./temperatura');
 
 test('deve converter 0°C para 32°F', () => {
-    expect(celsiusParaFahrenheit(0)).toBe(80);
+    expect(celsiusParaFahrenheit(0)).toBe(32);
 });
 
 test('deve converter 100°C para 212°F', () => {
